781c03dcc651310b0c19ac28f5148ac11444b393
James Percival <j.percival@imperial.ac.uk>
2015-06-01 22:27:11 +0100

    GMSHreader now working.

--- GMSHreader/GMSHreader.cxx
+++ GMSHreader/GMSHreader.cxx
@@ -49,9 +49,95 @@ int GMSHreader::RequestData(
   ifstream GMSHfile;
   GMSHfile.open(this->FileName);
 
+  std::string line;
+
+
+  // skip the header
+
+  this->DebugOff();
+
+
+for(GMSHfile >> line;line.compare("$Nodes");GMSHfile >> line){
+  vtkDebugMacro(<< line);
+}
   // try to read the point data
+  int n;
+  GMSHfile >> n;
+  vtkSmartPointer<vtkPoints> outpoints= vtkSmartPointer<vtkPoints>::New();
+  outpoints->Allocate(n);
+  for (int i=0;i<n;i++){
+    int nodeno;
+    double x,y,z;
+    GMSHfile >> nodeno >> x >> y >> z;
+    outpoints->InsertNextPoint(x,y,z);
+  }
+
+  output->SetPoints(outpoints);
 
   // try to read the element data
+for(GMSHfile >> line;line.compare("$Elements");GMSHfile >> line){
+  vtkDebugMacro(<< line);
+}
+  GMSHfile >> n;
+  output->Allocate(n);
+  int eleNo, CellType, tagNo;
+  for (int i=0;i<n;i++){
+    GMSHfile>> eleNo >> CellType >> tagNo;
+    for (int j=0;j<tagNo;j++){
+      int tag;
+      GMSHfile >> tag;
+    }
+    vtkDebugMacro(<< CellType);
+    switch (CellType)
+    { 
+    case 15:
+      {
+      // Point
+      int cellPoints[1];
+      vtkIdType cellPointsVTK[1];
+      GMSHfile>> cellPoints[0];
+for(int j=0;j<1;j++){
+       cellPointsVTK[j]=cellPoints[j]-1;
+      }
+      output->InsertNextCell((int)1,(vtkIdType)1,cellPointsVTK);
+      }
+ case 1:
+// Line
+ {int cellPoints[2];
+      vtkIdType cellPointsVTK[2];
+GMSHfile>> cellPoints[0] >> cellPoints[1];
+for(int j=0;j<2;j++){
+       cellPointsVTK[j]=cellPoints[j]-1;
+    }
+      output->InsertNextCell((int)3,(vtkIdType)2,cellPointsVTK);
+break;
+}
+ case 2:
+ // Triangle
+ {
+int cellPoints[3];
+      vtkIdType cellPointsVTK[3];
+GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2];
+for(int j=0;j<3;j++){
+       cellPointsVTK[j]=cellPoints[j]-1;
+    }
+      output->InsertNextCell((int)5,(vtkIdType)3,cellPointsVTK);
+break;
+}
+case 4:
+// Tetrahedron
+{
+      int cellPoints[4];
+      vtkIdType cellPointsVTK[4];
+GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2] >> cellPoints[3];
+for(int j=0;j<4;j++){
+       cellPointsVTK[j]=cellPoints[j]-1;
+    }
+      output->InsertNextCell((int)10,(vtkIdType)4,cellPointsVTK);
+break;
+}
+}
+}
 
   // try to close the GMSH file
   GMSHfile.close();
