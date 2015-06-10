62f205c3a22176d7f9e133e2213e6e225530f0c4
James Percival <j.percival@imperial.ac.uk>
2015-06-10 10:06:35 +0100

    BUGFIX: Sorting the order of physical ids and elementary entities in gmsh file reader

--- FLMLreader/FLMLreader.cxx
+++ FLMLreader/FLMLreader.cxx
@@ -10,6 +10,8 @@
 #include "vtkCellData.h"
 #include "vtkIdTypeArray.h"
 #include "vtkCellArray.h"
+#include "vtkDataArray.h"
+#include "vtkIntArray.h"
 #include "vtkDoubleArray.h"
 #include "vtkPoints.h"
 #include "vtkPointData.h"
@@ -23,6 +25,8 @@
 #include "vtkQuad.h"
 #include "vtkHexahedron.h"
 #include "vtkPolygon.h"
+#include "vtkMath.h"
+
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -103,6 +107,8 @@ output1->DeepCopy(bcs->GetOutput(1));
 
 vtkDebugMacro(<<"Linked to output");
 
+ vtkIntArray* boundaryIds=vtkIntArray::SafeDownCast(output1->GetCellData()->GetArray("PhysicalIds"));
+
 lhs="/material_phase[";
 rhs="]/";
 for (int i=0;i<Spud::option_count("/material_phase");i++){
@@ -119,9 +125,42 @@ for (int i=0;i<Spud::option_count("/material_phase");i++){
     std::string field_name;
     Spud::get_option(key2+"name",field_name);
     vtkDebugMacro(<<i<<" "<<j<<" "<<key2.c_str());
-    scalar_field->SetName(field_name.c_str());
+
+    std::map<int,int> id2bc;
+    for (int k=0;k<Spud::option_count(key2+"prognostic/boundary_conditions");k++){
+      std::stringstream c;
+      c<<k;
+      std::string key3=key2+"prognostic/boundary_conditions["+c.str()+"]/surface_ids";
+      std::vector<int> surface_ids;
+      Spud::get_option(key3,surface_ids);
+      for (std::vector<int>::iterator it = surface_ids.begin(); it!=surface_ids.end(); ++it){
+	id2bc.insert(std::pair<int,int>(*it,k));
+vtkDebugMacro(<<"BC pair: "<< *it <<" "<< k);
+      }
+    }
+
+    scalar_field->SetName((state_name+"::"+field_name).c_str());
     scalar_field->Allocate(output0->GetNumberOfPoints());
     output0->GetPointData()->AddArray(scalar_field);
+    scalar_field= vtkSmartPointer<vtkDoubleArray>::New();
+    scalar_field->SetName((state_name+"::"+field_name).c_str());
+    scalar_field->Allocate(output1->GetNumberOfPoints());
+    output1->GetPointData()->AddArray(scalar_field);
+    for (int k=0;k<output1->GetNumberOfCells();k++){
+      int id=boundaryIds->GetValue(k);
+      std::map<int,int>::iterator it;
+      it = id2bc.find(id);
+vtkDebugMacro(<<"BC val: "<< id <<" "<< k<<" "<<it->first);
+      if (it != id2bc.end()) {
+	for (int n=0;n<output1->GetCell(k)->GetNumberOfPoints();n++){
+scalar_field->SetValue(output1->GetCell(k)->GetPointId(n),0.0);
+	}
+      } else {
+	for (int n=0;n<output1->GetCell(k)->GetNumberOfPoints();n++){
+	  scalar_field->SetValue(output1->GetCell(k)->GetPointId(n),vtkMath::Nan());
+	}
+      }
+    }
   }
   for (int j=0;j<Spud::option_count(key+"vector_field");j++){
     vtkSmartPointer<vtkDoubleArray> vector_field= vtkSmartPointer<vtkDoubleArray>::New();
@@ -131,10 +170,23 @@ for (int i=0;i<Spud::option_count("/material_phase");i++){
     std::string field_name;
     Spud::get_option(key2+"name",field_name);
     vtkDebugMacro(<<i<<" "<<j<<" "<<key2.c_str());
-    vector_field->SetName(field_name.c_str());
+    vector_field->SetName((state_name+"::"+field_name).c_str());
     vector_field->SetNumberOfComponents(3);
     vector_field->Allocate(output0->GetNumberOfPoints());
     output0->GetPointData()->AddArray(vector_field);
+  }
+  for (int j=0;j<Spud::option_count(key+"tensor_field");j++){
+    vtkSmartPointer<vtkDoubleArray> tensor_field= vtkSmartPointer<vtkDoubleArray>::New();
+    std::stringstream b;
+    b<<j;
+    std::string key2=key+"tensor_field["+b.str()+rhs;
+    std::string field_name;
+    Spud::get_option(key2+"name",field_name);
+    vtkDebugMacro(<<i<<" "<<j<<" "<<key2.c_str());
+    tensor_field->SetName((state_name+"::"+field_name).c_str());
+    tensor_field->SetNumberOfComponents(9);
+    tensor_field->Allocate(output0->GetNumberOfPoints());
+    output0->GetPointData()->AddArray(tensor_field);
   }
  }
 
