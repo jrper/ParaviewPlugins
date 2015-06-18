9b20bd2e5dcae79d17876998581ab529cd44f86b
James Percival <j.percival@imperial.ac.uk>
2015-06-18 21:47:33 +0100

    Adding support for binary files to GMSHreader. Not yet endian safe.

--- GMSHreader/GMSHreader.cxx
+++ GMSHreader/GMSHreader.cxx
@@ -24,7 +24,6 @@
 #include <map>
 
 
-
 vtkCxxRevisionMacro(GMSHreader, "$Revision: 0.0$");
 vtkStandardNewMacro(GMSHreader);
 
@@ -36,6 +35,192 @@ GMSHreader::GMSHreader(){
 GMSHreader::~GMSHreader(){
  this->SetFileName(0);
 };
+
+int readGMSHheader(std::ifstream& GMSHfile,float &version,int& isBinary,int& isSameEndian)
+{
+  std::string line;
+  GMSHfile >> line;
+  if(line.compare("$MeshFormat")) return 0;
+  GMSHfile >> version;
+  GMSHfile >> isBinary;
+  int datasize;
+  GMSHfile >> datasize; 
+  if (datasize != sizeof(double)) return 0;
+  if (isBinary==1) {
+    int one;
+    GMSHfile.read(( char*) &one,4);
+    isSameEndian=(one==1);
+    GMSHfile.read(( char*) &one,1);
+  } 
+  GMSHfile >> line;
+  if(line.compare("$EndMeshFormat")) return 0;
+  return int(version>2.1);
+}
+
+
+      int readGMSHnodes(std::ifstream& GMSHfile,vtkPoints* outpoints,int isBinary,int isSameEndian,std::map<int,vtkIdType>& nodeMap)
+{
+  std::string line;
+  GMSHfile >> line;
+  if(line.compare("$Nodes")) return 0;
+  int nnodes;
+  GMSHfile >> nnodes;
+  outpoints->Allocate(nnodes);
+  if (isBinary) {
+    char tim;
+    GMSHfile.read(&tim,1);
+    for(vtkIdType i=0;i<nnodes;i++) {
+      int nodeno;
+      double x,y,z;
+      GMSHfile.read(( char*) &nodeno,4);
+      GMSHfile.read(( char*) &x,sizeof(double));
+      GMSHfile.read(( char*) &y,sizeof(double));
+      GMSHfile.read(( char*) &z,sizeof(double));
+
+      nodeMap[nodeno]=i;
+      outpoints->InsertNextPoint(x,y,z);
+    }
+  } else {
+    for(vtkIdType i=0;i<nnodes;i++) {
+      int nodeno;
+      double x,y,z;
+      GMSHfile >> nodeno >> x >> y >> z;
+      nodeMap[nodeno]=i;
+      outpoints->InsertNextPoint(x,y,z);
+    }
+  }
+  GMSHfile >> line;
+  if(line.compare("$EndNodes")) return 0;
+  return 1;
+}
+
+int readGMSHelements(std::ifstream& GMSHfile,vtkUnstructuredGrid* output,int isBinary,int isSameEndian,std::map<int,vtkIdType>& nodeMap)
+{ 
+  const int nodeCount[20]={-1,2,3,4,4,8,6,5,3,6,9,10,
+			   27,18,14,1,8,20,15,13};
+  const int VTKtype[20]={-1,3,5,4,10,8,6,5,3,6,9,10,
+			   27,18,14,15,8,20,15,13};
+  std::string line;
+  GMSHfile >> line;
+  if(line.compare("$Elements")) return 0;
+  vtkIdType nelements;
+  GMSHfile >> nelements;
+  output->Allocate(nelements);
+  vtkSmartPointer<vtkIntArray> ElementaryEntities= vtkSmartPointer<vtkIntArray>::New();
+  ElementaryEntities->SetName("ElementaryEntities");
+  ElementaryEntities->Allocate(nelements);
+  vtkSmartPointer<vtkIntArray> PhysicalIds= vtkSmartPointer<vtkIntArray>::New();
+  PhysicalIds->SetName("PhysicalIds");
+  PhysicalIds->Allocate(nelements);
+  if (isBinary) {
+    char tim;
+    GMSHfile.read(( char*) &tim,1);
+    int n=0;
+    while(n<nelements) {
+      int eleType,nElements,nTags;
+      GMSHfile.read(( char*) &eleType,4);
+      GMSHfile.read(( char*) &nElements,4);
+      GMSHfile.read(( char*) &nTags,4);
+      n+=nElements;
+      for (int i=0;i<nElements;i++) {
+	int nele;
+	GMSHfile.read(( char*) &nele,4);
+	for (int j=0;j<nTags;j++) {
+	  int tag;
+	  GMSHfile.read(( char*) &tag,4);
+	  if (j==1) {
+	    ElementaryEntities->InsertNextValue(tag);
+	  }
+	  if (j==0) {
+	    PhysicalIds->InsertNextValue(tag);
+	  }    
+	}
+	std::vector<int> enodes;
+	enodes.reserve(eleType);
+	int node[nodeCount[eleType]];
+	GMSHfile.read(( char*) &node[0],4*nodeCount[eleType]);
+	vtkIdType cellPoints[nodeCount[eleType]];
+	for (int j=0;j<nodeCount[eleType];j++) {
+	  cellPoints[j]=nodeMap[node[j]];
+	}
+	output->InsertNextCell(VTKtype[eleType],nodeCount[eleType],cellPoints);
+      }
+    }
+  } else {
+    int eleNo, CellType,tagNo;
+    for (vtkIdType i=0;i<nelements;i++){
+      GMSHfile>> eleNo >> CellType >> tagNo;
+      for (int j=0;j<tagNo;j++){
+	int tag;
+	GMSHfile >> tag;
+	if (j==1) {
+	  ElementaryEntities->InsertNextValue(tag);
+	}
+	if (j==0) {
+	  PhysicalIds->InsertNextValue(tag);
+	}      
+      }
+      switch (CellType)
+	{ 
+	case 15:
+	  {
+	    // Point
+	    int cellPoints[1];
+	    vtkIdType cellPointsVTK[1];
+	    GMSHfile>> cellPoints[0];
+	    for(int j=0;j<1;j++){
+	      cellPointsVTK[j]=nodeMap[cellPoints[j]];
+	    }
+	    output->InsertNextCell((int)1,(vtkIdType)1,cellPointsVTK);
+	    break;
+	  }
+	case 1:
+	  // Line
+	  {
+	    int cellPoints[2];
+	    vtkIdType cellPointsVTK[2];
+	    GMSHfile>> cellPoints[0] >> cellPoints[1];
+	    for(int j=0;j<2;j++){
+	      cellPointsVTK[j]=nodeMap[cellPoints[j]];
+	    }
+	    output->InsertNextCell((int)3,(vtkIdType)2,cellPointsVTK);
+	    break;
+	  }
+	case 2:
+	  // Triangle
+	  {
+	    int cellPoints[3];
+	    vtkIdType cellPointsVTK[3];
+	    GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2];
+	    for(int j=0;j<3;j++){
+	      cellPointsVTK[j]=nodeMap[cellPoints[j]];
+	    }
+	    output->InsertNextCell((int)5,(vtkIdType)3,cellPointsVTK);
+	    break;
+	  }
+	case 4:
+	  // Tetrahedron
+	  {
+	    int cellPoints[4];
+	    vtkIdType cellPointsVTK[4];
+	    GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2] >> cellPoints[3];
+	    for(int j=0;j<4;j++){
+	      cellPointsVTK[j]=nodeMap[cellPoints[j]];
+	    }
+	    output->InsertNextCell((int)10,(vtkIdType)4,cellPointsVTK);
+	    break;
+	  }
+	}
+    }
+
+    output->GetCellData()->AddArray(ElementaryEntities);
+    output->GetCellData()->AddArray(PhysicalIds);
+
+  }
+  GMSHfile >> line;
+  if(line.compare("$EndElements")) return 0;
+  return 1;
+}
 
 int GMSHreader::RequestData(
 		      vtkInformation* vtkNotUsed(request),
@@ -50,111 +235,25 @@ int GMSHreader::RequestData(
   ifstream GMSHfile;
   GMSHfile.open(this->FileName);
 
-  std::string line;
-
-
-  // skip the header
-
   this->DebugOff();
 
+  float version=-66666;
+  int isBinary=-66666;
+  int isSameEndian=-66666;
 
-for(GMSHfile >> line;line.compare("$Nodes");GMSHfile >> line){
-  vtkDebugMacro(<< line);
-}
-  // try to read the point data
-  int n;
-  GMSHfile >> n;
+  // try to read the header
+  // for now we assume that the isSameEndian flag is true, otherwise we'll
+  
+  readGMSHheader(GMSHfile,version,isBinary,isSameEndian);
+
+  std::map<int,vtkIdType>nodeMap;
   vtkSmartPointer<vtkPoints> outpoints= vtkSmartPointer<vtkPoints>::New();
-  outpoints->Allocate(n);
-  for (int i=0;i<n;i++){
-    int nodeno;
-    double x,y,z;
-    GMSHfile >> nodeno >> x >> y >> z;
-    outpoints->InsertNextPoint(x,y,z);
-  }
-
+  // try to read the point data
+  readGMSHnodes(GMSHfile,outpoints,isBinary,isSameEndian,nodeMap);
   output->SetPoints(outpoints);
 
   // try to read the element data
-for(GMSHfile >> line;line.compare("$Elements");GMSHfile >> line){
-  vtkDebugMacro(<< line);
-}
-  GMSHfile >> n;
-  output->Allocate(n);
-  int eleNo, CellType, tagNo;
-
-  vtkSmartPointer<vtkIntArray> ElementaryEntities= vtkSmartPointer<vtkIntArray>::New();
-  ElementaryEntities->SetName("ElementaryEntities");
-  ElementaryEntities->Allocate(n);
-  vtkSmartPointer<vtkIntArray> PhysicalIds= vtkSmartPointer<vtkIntArray>::New();
-  PhysicalIds->SetName("PhysicalIds");
-  PhysicalIds->Allocate(n);
-  for (int i=0;i<n;i++){
-    GMSHfile>> eleNo >> CellType >> tagNo;
-    for (int j=0;j<tagNo;j++){
-      int tag;
-      GMSHfile >> tag;
-      if (j==0) {
-	ElementaryEntities->InsertNextValue(tag);
-      }
-      if (j==1) {
-	PhysicalIds->InsertNextValue(tag);
-      }      
-    }
-    vtkDebugMacro(<< CellType);
-    switch (CellType)
-    { 
-    case 15:
-      {
-      // Point
-      int cellPoints[1];
-      vtkIdType cellPointsVTK[1];
-      GMSHfile>> cellPoints[0];
-for(int j=0;j<1;j++){
-       cellPointsVTK[j]=cellPoints[j]-1;
-      }
-      output->InsertNextCell((int)1,(vtkIdType)1,cellPointsVTK);
-      }
- case 1:
-// Line
- {int cellPoints[2];
-      vtkIdType cellPointsVTK[2];
-GMSHfile>> cellPoints[0] >> cellPoints[1];
-for(int j=0;j<2;j++){
-       cellPointsVTK[j]=cellPoints[j]-1;
-    }
-      output->InsertNextCell((int)3,(vtkIdType)2,cellPointsVTK);
-break;
-}
- case 2:
- // Triangle
- {
-int cellPoints[3];
-      vtkIdType cellPointsVTK[3];
-GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2];
-for(int j=0;j<3;j++){
-       cellPointsVTK[j]=cellPoints[j]-1;
-    }
-      output->InsertNextCell((int)5,(vtkIdType)3,cellPointsVTK);
-break;
-}
-case 4:
-// Tetrahedron
-{
-      int cellPoints[4];
-      vtkIdType cellPointsVTK[4];
-GMSHfile>> cellPoints[0] >> cellPoints[1] >> cellPoints[2] >> cellPoints[3];
-for(int j=0;j<4;j++){
-       cellPointsVTK[j]=cellPoints[j]-1;
-    }
-      output->InsertNextCell((int)10,(vtkIdType)4,cellPointsVTK);
-break;
-}
-}
-}
-
-  output->GetCellData()->AddArray(ElementaryEntities);
-  output->GetCellData()->AddArray(PhysicalIds);
+  readGMSHelements(GMSHfile,output,isBinary,isSameEndian,nodeMap);
 
   // try to close the GMSH file
   GMSHfile.close();
