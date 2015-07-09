6ec72abc2247870bf4c1001c70baf80e05ad29ed
James Percival <j.percival@imperial.ac.uk>
2015-07-09 14:57:30 +0100

    Adding parallel support for pvtus

---
+++ TemporalXMLUnstructuredGridReader/TemporalXMLPUnstructuredGridReader.cxx
@@ -0,0 +1,121 @@
+#include "TemporalXMLPUnstructuredGridReader.h"
+
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkPointData.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkXMLUnstructuredGridReader.h"
+#include "vtkObjectFactory.h"
+#include "vtkSmartPointer.h"
+#include "vtkXMLDataReader.h"
+#include "vtkXMLDataParser.h"
+
+#include <iostream>
+
+vtkCxxRevisionMacro(TemporalXMLPUnstructuredGridReader, "$Revision: 0.0$");
+vtkStandardNewMacro(TemporalXMLPUnstructuredGridReader);
+
+TemporalXMLPUnstructuredGridReader::TemporalXMLPUnstructuredGridReader(){
+  this->FileName=NULL;
+  this->SetNumberOfInputPorts(0);
+  this->SetNumberOfOutputPorts(1);
+};
+TemporalXMLPUnstructuredGridReader::~TemporalXMLPUnstructuredGridReader(){
+ this->SetFileName(0);
+};
+
+int TemporalXMLPUnstructuredGridReader::RequestInformation(
+		      vtkInformation* request,
+		      vtkInformationVector **inputVector,
+		      vtkInformationVector* outputVector )
+{
+  vtkInformation* outInfo=outputVector->GetInformationObject(0);
+  {
+    this->Superclass::RequestInformation(
+		      request,
+		      inputVector,
+		      outputVector );
+
+
+    double time;
+    int ntimes;
+;
+    vtkXMLDataElement* eVTKFile=this->GetXMLParser()->GetRootElement();
+    const char* name = this->GetDataSetName();
+    int i;
+    vtkXMLDataElement* ePrimary = 0;
+    for(i=0; i < eVTKFile->GetNumberOfNestedElements(); ++i)
+      {
+	vtkXMLDataElement* eNested = eVTKFile->GetNestedElement(i);
+	if(strcmp(eNested->GetName(), name) == 0)
+	  {
+	    ePrimary = eNested;
+	    break;
+	  }
+      }
+    for(i=0; i < ePrimary->GetNumberOfNestedElements(); ++i)
+      {
+	vtkXMLDataElement* eNested = ePrimary->GetNestedElement(i);
+	if(strcmp(eNested->GetName(), "Piece") == 0)
+	  {
+	    ePrimary = eNested;
+	    break;
+	  }
+      }
+    const char* fileName = ePrimary->GetAttribute("Source");
+    vtkSmartPointer<vtkXMLUnstructuredGridReader> reader = vtkXMLUnstructuredGridReader::New();
+    reader->SetFileName(fileName);
+    reader->UpdateInformation();
+    eVTKFile=reader->GetXMLParser()->GetRootElement();
+    for(i=0; i < eVTKFile->GetNumberOfNestedElements(); ++i)
+      {
+    vtkXMLDataElement* eNested = eVTKFile->GetNestedElement(i);
+    if(strcmp(eNested->GetName(), "UnstructuredGrid") == 0)
+      {
+      ePrimary = eNested;
+      break;
+      }
+    }
+  ntimes=ePrimary->GetScalarAttribute("Time",time);
+  if (ntimes==0) {
+    for(i=0; i < ePrimary->GetNumberOfNestedElements(); ++i) {
+      vtkXMLDataElement* eNested = ePrimary->GetNestedElement(i);
+      if(strcmp(eNested->GetName(), "Piece") == 0) {
+	ePrimary = eNested;
+	break;
+      }
+    }
+    for(i=0; i < ePrimary->GetNumberOfNestedElements(); ++i) {
+      vtkXMLDataElement* eNested = ePrimary->GetNestedElement(i);
+      if(strcmp(eNested->GetName(), "PointData") == 0) {
+	ePrimary = eNested;
+	break;
+      }
+    }
+    for(i=0; i < ePrimary->GetNumberOfNestedElements(); ++i) {
+      vtkXMLDataElement* eNested = ePrimary->GetNestedElement(i);
+      const char* name=eNested->GetAttribute("Name");
+      if(strcmp(eNested->GetName(), "DataArray") == 0
+	   && (strcmp(name,"Time") == 0 
+		 || (strlen(name)>=6
+		       && strcmp(&name[strlen(name)-6],"::Time") == 0))){
+	ePrimary = eNested;
+	break;
+      } 
+    }
+    ntimes=ePrimary->GetScalarAttribute("RangeMax",time);
+  }
+if (ntimes>0)  outInfo->Set(vtkStreamingDemandDrivenPipeline::TIME_STEPS(),&time,1);
+  }
+
+  return 1;
+}
+
+void TemporalXMLPUnstructuredGridReader::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+ 
+  os << indent << "File Name: "
+      << (this->FileName ? this->FileName : "(none)") << "\n";
+}
---
+++ TemporalXMLUnstructuredGridReader/TemporalXMLPUnstructuredGridReader.h
@@ -0,0 +1,25 @@
+#include "vtkXMLPUnstructuredGridReader.h"
+
+class TemporalXMLPUnstructuredGridReader : public vtkXMLPUnstructuredGridReader
+{
+public:
+  static TemporalXMLPUnstructuredGridReader *New();
+  vtkTypeMacro(TemporalXMLPUnstructuredGridReader,vtkXMLPUnstructuredGridReader);
+
+  void PrintSelf(ostream& os, vtkIndent indent);
+ protected:
+
+  TemporalXMLPUnstructuredGridReader();
+  ~TemporalXMLPUnstructuredGridReader();
+
+  virtual int RequestInformation(
+			  vtkInformation* request,
+			  vtkInformationVector** InputVector,
+			  vtkInformationVector* OutputVector);
+
+ private:
+  TemporalXMLPUnstructuredGridReader(const TemporalXMLPUnstructuredGridReader&);  // Not implemented.
+  void operator=(const TemporalXMLPUnstructuredGridReader&);  // Not implemented.
+
+};
+
