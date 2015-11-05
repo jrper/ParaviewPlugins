6835fbc3526dfbfb3f2a8e45364ae430f90aa119
James Percival <j.percival@imperial.ac.uk>
2015-11-05 16:08:34 +0000

    Add missing TemporaXMLPolyDataReader files.

---
+++ TemporalDataReaders/TemporalXMLPolyDataReader.h
@@ -0,0 +1,25 @@
+#include "vtkXMLPolyDataReader.h"
+
+class TemporalXMLPolyDataReader : public vtkXMLPolyDataReader
+{
+public:
+  static TemporalXMLPolyDataReader *New();
+  vtkTypeMacro(TemporalXMLPolyDataReader,vtkXMLPolyDataReader);
+
+  void PrintSelf(ostream& os, vtkIndent indent);
+ protected:
+
+  TemporalXMLPolyDataReader();
+  ~TemporalXMLPolyDataReader();
+
+  virtual int RequestInformation(
+			  vtkInformation* request,
+			  vtkInformationVector** InputVector,
+			  vtkInformationVector* OutputVector);
+
+ private:
+  TemporalXMLPolyDataReader(const TemporalXMLPolyDataReader&);  // Not implemented.
+  void operator=(const TemporalXMLPolyDataReader&);  // Not implemented.
+
+};
+
